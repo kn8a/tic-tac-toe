271d266813b6a14394357d9e885ae1dea62c8ba6
kn8a <mydreamhomefl@gmail.com>
2022-01-28 04:36:28 -0500

    Check for winner/tie works

--- game.js
+++ game.js
@@ -1,37 +1,37 @@
 const log = console.log;
 
 const Gameboard = (()=>{
-    // same thing, just easier to type :D
+    
     const board = new Array(9).fill('');
-    let player = "x";
-    
-    
-    
+    let player = "x";    
     const cells = document.querySelectorAll(".square");
-    log(cells);
+    
     cells.forEach((cell) => {
         cell.addEventListener('click', () => {
             let choice = cell.getAttribute('data-cell');
-            log(choice);
             if (board[choice] == "x" || board[choice] == "o") {return} //check if cell already marked
             //continue here checking for who's turn and placing mark
             if (player == 'x') {
                 const xMark = document.createElement('img');
                 xMark.src = './svg/x.svg';
                 document.getElementById(`s${choice}`).appendChild(xMark);
+                cell.classList.add('marked');
                 board[choice] = 'x';
+                checkForWinner(board, player);
                 player = 'o';
             }
-            else {
+            else if (player == 'o') {
                 const yMark = document.createElement('img');
                 yMark.src = './svg/o.svg';
                 document.getElementById(`s${choice}`).appendChild(yMark);
+                cell.classList.add('marked');
                 board[choice] = 'o';
+                checkForWinner(board, player);
                 player = 'x';
             }
-            let winner = checkForWinner(board, player) //winner function call
+            //let winner = checkForWinner(board, player) //winner function call
 
-            log(board);
+            log(board.find(empty => empty ==''));
 
         })
     }) 
@@ -40,17 +40,23 @@ const Gameboard = (()=>{
 
     //const checkCell = 
     function checkForWinner(a, player) {
-        if (a[0] == a[1] && a[0] == a[2] ||
-            a[2] == a[3] && a[2] == a[4] ||
-            a[6] == a[7] && a[6] == a[8] ||
-            a[0] == a[3] && a[0] == a[6] ||
-            a[1] == a[4] && a[1] == a[7] ||
-            a[2] == a[5] && a[2] == a[8] ||
-            a[2] == a[4] && a[2] == a[6] ||
-            a[0] == a[4] && a[0] == a[8]) {
-                announceWinner(player);        
+        let p = player;
+        //0=1=2    2=3=4    6=7=8      0=3=6    1=4=7    2=5=8      2=4=6    0=4=8
+        if (a[0] == p && a[1] == p && a[2] == p||
+            a[3] == p && a[4] == p && a[5] == p||
+            a[6] == p && a[7] == p && a[8] == p||
+            a[0] == p && a[3] == p && a[6] == p||
+            a[1] == p && a[4] == p && a[7] == p||
+            a[2] == p && a[5] == p && a[8] == p||
+            a[0] == p && a[4] == p && a[8] == p||
+            a[2] == p && a[4] == p && a[6] == p) {
+            window.alert(player + " is the winner");        
+        }
+        else if (board.find(empty => empty =='') == undefined) {
+            window.alert("its a tie");
         }
     }
+
 
     //function checkCell(cell, array)
     
